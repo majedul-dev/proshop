599fc02ee0124c4dcae930fadf64856bbfea00bb
majedul-dev <majedulislam956@gmail.com>
2020-11-10 15:21:49 +0600

    Deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://majed:majed@cluster0.rfloa.mongodb.net/proshop?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
