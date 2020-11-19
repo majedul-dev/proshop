7c9433b0e2072296c51b848b32e04d6fa5c321bd
majedul-dev <majedulislam956@gmail.com>
2020-11-19 13:28:47 +0600

    make it better

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { errorHandler, notFoound } from "./middleware/errorHandler.js";
+import path from "path";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+app.use(notFoound);
+app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on PORT ${PORT}`);
+});
